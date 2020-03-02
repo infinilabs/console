1549da97f72e429da5e67939e67b02b5fdd0dbe4
medcl <m@medcl.net>
2020-03-03 00:02:33 +0800

    add qrcode, test ci

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,7 +1,12 @@
 export default () => {
     var md5 = require('md5');
- 
     console.log(md5('message'));
+    
+    var QRCode = require('qrcode')
+    QRCode.toDataURL('some text', { version: 2 }, function (err, url) {
+  console.log(url)
+})
+
 
     return <div>hello 极限科技</div>;
 }

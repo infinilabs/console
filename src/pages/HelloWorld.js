2bd9ab9e3c3a34918ec79cad00dd3474a4dd0735
medcl <m@medcl.net>
2020-03-03 00:10:25 +0800

    remove md5, test ci

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,7 +1,4 @@
 export default () => {
-    var md5 = require('md5');
-    console.log(md5('message'));
-    
     var QRCode = require('qrcode')
     QRCode.toDataURL('some text', { version: 2 }, function (err, url) {
   console.log(url)

17528cc096b5270f70d73b94b33f1d6807e7441a
medcl <m@medcl.net>
2020-03-22 00:08:59 +0800

    update mock

--- app/web/mock/endpoints/api.js
+++ app/web/mock/endpoints/api.js
@@ -4,12 +4,15 @@ const random_endpoints = [
       name: "LENOVO",
       ip: '192.168.3.1',
       status: "active", //active/inactive/unmonitored
-      last_active: "2020-03-21 11:12:33"
+      last_active: "2020-03-21 11:12:33",
+      tag: ["win10"]
     },
     {
       os: "Linux",
       name: 'RaspberryPi',
       ip: '192.168.3.81',
+      last_active: "2020-03-21 11:12:33",
+      tag: ["win10"],
       credentials:{
         user: "pi",
         password: "elastic"
@@ -21,6 +24,7 @@ const random_endpoints = [
   
   export default {
     'get /endpoints/get_endpoints': function (req, res) {
+      
       setTimeout(() => {
         res.json(random_endpoints);
       }, 3000);

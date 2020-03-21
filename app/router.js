d7fae18732989ba889c25ac18e21d83236d44b67
silenceqi <silenceqi@hotmail.com>
2020-03-21 22:44:48 +0800

    add ssh command test

--- app/router.js
+++ app/router.js
@@ -5,6 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  router.post('/command/exec', controller.command.exec);
   router.all('/api/*', controller.home.proxy);
   router.get('*', controller.home.index);
 };
--- config/plugin.js
+++ config/plugin.js
@@ -9,3 +9,8 @@ exports.nunjucks = {
   enable: true,
   package: 'egg-view-nunjucks',
 };
+
+exports.validate = {
+    enable: true,
+    package: 'egg-validate',
+};

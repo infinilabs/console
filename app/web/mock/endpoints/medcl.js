917b019290a594c7411be84bb059f776f1ef1182
medcl <m@medcl.net>
2020-03-21 17:54:31 +0800

    add mock data for endpoints

---
+++ app/web/mock/endpoints/medcl.js
@@ -0,0 +1,36 @@
+const random_endpoints = [
+    {
+      os: 'Windows',
+      name: "LENOVO",
+      ip: '192.168.3.1',
+      status: "active", //active/inactive/unmonitored
+      last_active: "2020-03-21 11:12:33"
+
+    },
+    {
+      os: 'Windows',
+      ip: '192.168.3.100',
+    },
+    {
+      os: 'Linux',
+      ip: '192.168.3.91',
+    },
+  ];
+  
+  let random_endpoint_pick = 0;
+  
+  export default {
+    'get /endpoints/get_endpoints': function (req, res) {
+      setTimeout(() => {
+        res.json(random_endpoints);
+      }, 3000);
+    },
+    'get /endpoints/get_endpoint/1': function (req, res) {
+      const responseObj = random_endpoints[random_endpoint_pick % random_endpoints.length];
+      random_endpoint_pick += 1;
+      setTimeout(() => {
+        res.json(responseObj);
+      }, 3000);
+    },
+  };
+  

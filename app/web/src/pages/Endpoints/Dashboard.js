b810477886828200a97b3d5c9afe390890081e6e
medcl <m@medcl.net>
2020-03-29 13:24:08 +0800

    remove unused ;

--- app/web/src/pages/Endpoints/Dashboard.js
+++ app/web/src/pages/Endpoints/Dashboard.js
@@ -235,7 +235,7 @@ class Dashboard extends React.Component {
                 dataSource={dataSource}
                 columns={columns}
                 size="small"
-              />;
+              />
             </div>
           </Col>
         </Row>

034b127be130b81998d23c361432bf64dd77d1b8
silenceqi <silenceqi@hotmail.com>
2020-11-12 22:36:16 +0800

    add ingest pipeline

--- web/src/services/datamanagement.js
+++ web/src/services/datamanagement.js
@@ -10,3 +10,29 @@ export async function saveLogstashConfig(config){
         body: config,
       });
 }
+
+// pipeline
+export async function getPipelines(params){
+    return request('/data/pipeline');
+}
+
+export async function addPipeline(params){
+    return request('/data/pipeline/add', {
+        method: 'POST',
+        body: params,
+      });
+}
+
+export async function updatePipeline(params){
+    return request('/data/pipeline/update', {
+        method: 'PUT',
+        body: params,
+      });
+}
+export async function deletePipeline(params){
+    return request('/data/pipeline', {
+        method: 'POST',
+        body: params,
+    });
+}
+//end pipeline

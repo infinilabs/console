88ed67a9913d7353fec7779a0316bbbfc7da93d1
shiyang <liaosy21@gmail.com>
2020-11-23 00:46:41 +0800

    add SearchMonitor userStat

--- web/src/pages/Dashboard/SearchMonitor.js
+++ web/src/pages/Dashboard/SearchMonitor.js
@@ -31,6 +31,7 @@ import {
 } from 'bizcharts';
 import DataSet from '@antv/data-set';
 import Slider from 'bizcharts-plugin-slider';
+import UserStat from './Search/UserStat';
 import TagCloud from './Search/TagCloud';
 
 import styles from "./Monitor.less";
@@ -43,19 +44,31 @@ class SearchMonitor extends PureComponent {
     return (
         <GridContent>
           <Row gutter={24}>
-            {/*<Col xl={6} lg={12} sm={24} xs={24}>*/}
-            {/*  <Card*/}
-            {/*      title="待定"*/}
-            {/*      loading={false}*/}
-            {/*      bordered={false}*/}
-            {/*      bodyStyle={{ overflow: 'hidden' }}*/}
-            {/*  >*/}
-            {/*    <h1>待定</h1>*/}
-            {/*  </Card>*/}
-            {/*</Col>*/}
-            <Col xl={6} lg={12} sm={24} xs={24}>
+            <Col xl={8} lg={12} md={24} sm={24} xs={24}>
+              <Card
+                  title="用户统计"
+                  loading={false}
+                  bordered={false}
+                  bodyStyle={{ overflow: 'hidden' }}
+              >
+                <UserStat></UserStat>
+              </Card>
+            </Col>
+            <Col xl={8} lg={12} md={24} sm={24} xs={24}>
               <Card
                   title="热门搜索"
+                  loading={false}
+                  bordered={false}
+                  bodyStyle={{ overflow: 'hidden' }}
+              >
+                <TagCloud>
+
+                </TagCloud>
+              </Card>
+            </Col>
+            <Col xl={8} lg={12} md={24} sm={24} xs={24}>
+              <Card
+                  title="新词"
                   loading={false}
                   bordered={false}
                   bodyStyle={{ overflow: 'hidden' }}

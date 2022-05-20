6571d75a21446e7355a732a616b8736ec69be0ec
liugq <silenceqi@hotmail.com>
2022-05-20 16:33:35 +0800

    add auth to gateway instance api

--- plugin/api/gateway/api.go
+++ plugin/api/gateway/api.go
@@ -6,6 +6,7 @@ package gateway
 
 import (
 	"infini.sh/framework/core/api"
+	"infini.sh/framework/core/api/rbac/enum"
 )
 
 type GatewayAPI struct {
@@ -15,12 +16,12 @@ type GatewayAPI struct {
 func init() {
 	gateway:=GatewayAPI{}
 	api.HandleAPIMethod(api.POST, "/gateway/instance/try_connect", gateway.tryConnect)
-	api.HandleAPIMethod(api.GET, "/gateway/instance/:instance_id", gateway.getInstance)
-	api.HandleAPIMethod(api.POST, "/gateway/instance", gateway.createInstance)
-	api.HandleAPIMethod(api.PUT, "/gateway/instance/:instance_id", gateway.updateInstance)
-	api.HandleAPIMethod(api.DELETE, "/gateway/instance/:instance_id", gateway.deleteInstance)
-	api.HandleAPIMethod(api.GET, "/gateway/instance/_search", gateway.searchInstance)
-	api.HandleAPIMethod(api.POST, "/gateway/instance/status", gateway.getInstanceStatus)
+	api.HandleAPIMethod(api.GET, "/gateway/instance/:instance_id", gateway.RequirePermission(gateway.getInstance, enum.PermissionGatewayInstanceRead))
+	api.HandleAPIMethod(api.POST, "/gateway/instance", gateway.RequirePermission(gateway.createInstance, enum.PermissionGatewayInstanceWrite))
+	api.HandleAPIMethod(api.PUT, "/gateway/instance/:instance_id", gateway.RequirePermission(gateway.updateInstance, enum.PermissionGatewayInstanceWrite))
+	api.HandleAPIMethod(api.DELETE, "/gateway/instance/:instance_id", gateway.RequirePermission(gateway.deleteInstance, enum.PermissionGatewayInstanceWrite))
+	api.HandleAPIMethod(api.GET, "/gateway/instance/_search", gateway.RequirePermission(gateway.searchInstance, enum.PermissionGatewayInstanceRead))
+	api.HandleAPIMethod(api.POST, "/gateway/instance/status", gateway.RequirePermission(gateway.getInstanceStatus, enum.PermissionGatewayInstanceRead))
 
-	api.HandleAPIMethod(api.POST, "/gateway/instance/:instance_id/_proxy", gateway.proxy)
+	api.HandleAPIMethod(api.POST, "/gateway/instance/:instance_id/_proxy", gateway.RequirePermission(gateway.proxy, enum.PermissionGatewayInstanceRead))
 }

e1471378ce44a869d3adb56cbdcb49ce3cb72830
liugq <silenceqi@hotmail.com>
2022-05-25 15:48:03 +0800

    fixed alerting bug

--- service/alerting/elasticsearch/engine.go
+++ service/alerting/elasticsearch/engine.go
@@ -495,7 +495,7 @@ func (engine *Engine) CheckCondition(rule *alerting.Rule)(*alerting.ConditionRes
 					"result": targetData.Data[dataKey][i][1],
 				})
 				if err != nil {
-					return nil, err
+					return nil, fmt.Errorf("evaluate rule [%s] error: %w", rule.ID, err)
 				}
 				if evaluateResult == true {
 					triggerCount += 1

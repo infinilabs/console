14f1f0ed652ffc96c27819bb01054d4adb4cddc3
liugq <silenceqi@hotmail.com>
2022-07-04 16:36:36 +0800

    regist channel schema

--- model/alerting/destination.go
+++ model/alerting/destination.go
@@ -9,9 +9,9 @@ import "infini.sh/framework/core/orm"
 type Channel struct {
 	orm.ORMObjectBase
 	Name string `json:"name" elastic_mapping:"name:{type:keyword,copy_to:search_text}"`
-	Type string `json:"type" elastic_mapping:"name:{type:keyword,copy_to:search_text}"` // email or webhook
+	Type string `json:"type" elastic_mapping:"type:{type:keyword,copy_to:search_text}"` // email or webhook
 	Priority       int `json:"priority,omitempty"`
-	Webhook *CustomWebhook `json:"webhook,omitempty"`
+	Webhook *CustomWebhook `json:"webhook,omitempty" elastic_mapping:"webhook:{type:object}"`
 	SearchText string `json:"-" elastic_mapping:"search_text:{type:text,index_prefixes:{},index_phrases:true, analyzer:suggest_text_search }"`
 }
 

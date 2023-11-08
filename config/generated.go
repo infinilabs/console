e9f76271f0c97be7717c1668076cdd225be98476
liugq <silenceqi@hotmail.com>
2023-11-08 09:46:25 +0800

    add sql api config (#222)
    Reviewed-on: https://git.infini.ltd/infini/console/pulls/222
    Co-authored-by: liugq <silenceqi@hotmail.com>
    Co-committed-by: liugq <silenceqi@hotmail.com>

--- config/generated.go
+++ config/generated.go
@@ -1,10 +1,10 @@
 package config
 
-const LastCommitLog = "90dc208fb713ff5fab513c0d29e6ce26825b4395"
-const BuildDate = "2023-08-05T09:14:11Z"
+const LastCommitLog = "N/A"
+const BuildDate = "N/A"
 
-const EOLDate  = "2023-12-31T10:10:10Z"
+const EOLDate = "N/A"
 
-const Version  = "1.0.0_SNAPSHOT"
+const Version = "0.0.1-SNAPSHOT"
 
-const BuildNumber  = "001"
+const BuildNumber = "001"

ed3a04a2bc8111d4364eb9cea73da350d3a6c58a
luohoufu <luohoufu@163.com>
2024-12-02 23:13:26 +0800

    chore: remove dict & reindex model

--- plugin/api/index_management/index.go
+++ plugin/api/index_management/index.go
@@ -1,19 +1,8 @@
 package index_management
 
 import (
-	"infini.sh/console/core"
-	"infini.sh/framework/core/elastic"
-	"infini.sh/framework/core/global"
-	"net/http"
-	"strconv"
-	"strings"
-	"time"
-
 	"infini.sh/console/config"
-	model2 "infini.sh/console/model"
-	httprouter "infini.sh/framework/core/api/router"
-	"infini.sh/framework/core/orm"
-	"infini.sh/framework/core/util"
+	"infini.sh/console/core"
 )
 
 type APIHandler struct {
@@ -21,100 +10,6 @@ type APIHandler struct {
 	core.Handler
 }
 
-func (handler APIHandler) GetDictListAction(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	var (
-		fromStr = handler.GetParameterOrDefault(req, "from", "0")
-		sizeStr = handler.GetParameterOrDefault(req, "size", "6")
-		tag     = handler.GetParameterOrDefault(req, "tags", "")
-		name    = handler.GetParameterOrDefault(req, "name", "")
-		from, _ = strconv.Atoi(fromStr)
-		size, _ = strconv.Atoi(sizeStr)
-		tags    = strings.Split(tag, ",")
-		resp    = newResponseBody()
-	)
-	if len(tags) > 3 {
-		tags = tags[0:3]
-	}
-	rel, err := model2.GetDictList(from, size, name, tags, global.MustLookupString(elastic.GlobalSystemElasticsearchID))
-	if err != nil {
-		resp["error"] = err
-		resp["status"] = false
-		handler.WriteJSON(w, resp, http.StatusOK)
-		return
-	}
-	resp["payload"] = rel
-	handler.WriteJSON(w, resp, http.StatusOK)
-}
-
-func (handler APIHandler) CreateDictItemAction(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	id := ps.ByName("id")
-	if strings.Trim(id, "/") == "" {
-		id = util.GetUUID()
-	}
-	createdAt := time.Now()
-
-	resp := newResponseBody()
-	dict := model2.Dict{
-		ID:        id,
-		CreatedAt: createdAt,
-		UpdatedAt: createdAt,
-	}
-	err := handler.DecodeJSON(req, &dict)
-	if err != nil {
-		resp["status"] = false
-		resp["error"] = err
-		handler.WriteJSON(w, resp, http.StatusOK)
-		return
-	}
-
-	err = orm.Create(nil, &dict)
-	if err != nil {
-		resp["status"] = false
-		resp["error"] = err
-		handler.WriteJSON(w, resp, http.StatusOK)
-		return
-	}
-	resp["payload"] = dict
-	handler.WriteJSON(w, resp, http.StatusOK)
-}
-
-func (handler APIHandler) DeleteDictItemAction(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	id := ps.ByName("id")
-	dict := model2.Dict{}
-	dict.ID = id
-	resp := newResponseBody()
-
-	err := orm.Delete(nil, dict)
-	if err != nil {
-		resp["status"] = false
-		resp["error"] = err
-		handler.WriteJSON(w, resp, http.StatusOK)
-		return
-	}
-	handler.WriteJSON(w, resp, http.StatusOK)
-}
-
-func (handler APIHandler) UpdateDictItemAction(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	dict := model2.Dict{}
-	err := handler.DecodeJSON(req, &dict)
-	resp := newResponseBody()
-	if err != nil {
-		resp["status"] = false
-		resp["error"] = err
-		handler.WriteJSON(w, resp, http.StatusOK)
-		return
-
-	}
-	dict.UpdatedAt = time.Now()
-
-	err = orm.Update(nil, dict)
-	if err != nil {
-		resp["status"] = false
-		resp["error"] = err
-		handler.WriteJSON(w, resp, http.StatusOK)
-		return
-	}
-	resp["payload"] = dict
-	handler.WriteJSON(w, resp, http.StatusOK)
-
+func newResponseBody() map[string]interface{} {
+	return map[string]interface{}{}
 }
--- plugin/api/init.go
+++ plugin/api/init.go
@@ -24,23 +24,12 @@ func Init(cfg *config.AppConfig) {
 	var pathPrefix = "/_search-center/"
 	var esPrefix = "/elasticsearch/:id/"
 	api.HandleAPIMethod(api.GET, path.Join(pathPrefix, "elasticsearch/overview"), handler.RequirePermission(handler.ElasticsearchOverviewAction, enum.PermissionElasticsearchMetricRead))
-	//api.HandleAPIMethod(api.POST, "/api/get_indices",index_management.API1)
-
-	api.HandleAPIMethod(api.GET, path.Join(pathPrefix, "dict/_search"), handler.GetDictListAction)
-	api.HandleAPIMethod(api.POST, path.Join(pathPrefix, "dict/*id"), handler.CreateDictItemAction)
-	//api.HandleAPIMethod(api.GET, "/api/dict/:id",handler.GetDictItemAction)
-	api.HandleAPIMethod(api.DELETE, path.Join(pathPrefix, "dict/:id"), handler.DeleteDictItemAction)
-	api.HandleAPIMethod(api.PUT, path.Join(pathPrefix, "dict/:id"), handler.UpdateDictItemAction)
 
 	api.HandleAPIMethod(api.POST, path.Join(esPrefix, "doc/:index/_search"), handler.RequireLogin(handler.HandleSearchDocumentAction))
 	api.HandleAPIMethod(api.POST, path.Join(esPrefix, "doc/:index"), handler.IndexRequired(handler.HandleAddDocumentAction, "doc.create"))
 	api.HandleAPIMethod(api.PUT, path.Join(esPrefix, "doc/:index/:docId"), handler.IndexRequired(handler.HandleUpdateDocumentAction, "doc.update"))
 	api.HandleAPIMethod(api.DELETE, path.Join(esPrefix, "doc/:index/:docId"), handler.IndexRequired(handler.HandleDeleteDocumentAction, "doc.delete"))
 	api.HandleAPIMethod(api.GET, path.Join(esPrefix, "doc/_validate"), handler.ValidateDocIDAction)
-
-	api.HandleAPIMethod(api.POST, path.Join(pathPrefix, "rebuild/*id"), handler.HandleReindexAction)
-	api.HandleAPIMethod(api.GET, path.Join(pathPrefix, "rebuild/_search"), handler.HandleGetRebuildListAction)
-	api.HandleAPIMethod(api.DELETE, path.Join(pathPrefix, "rebuild/:id"), handler.HandleDeleteRebuildAction)
 
 	api.HandleAPIMethod(api.GET, path.Join(esPrefix, "_cat/indices"), handler.RequireLogin(handler.HandleCatIndicesAction))
 	api.HandleAPIMethod(api.GET, path.Join(esPrefix, "index/:index/_mappings"), handler.IndexRequired(handler.HandleGetMappingsAction, "indices.get_mapping"))
@@ -56,15 +45,6 @@ func Init(cfg *config.AppConfig) {
 	api.HandleAPIMethod(api.DELETE, path.Join(pathPrefix, "elasticsearch/command/:cid"), handler.RequirePermission(handler.HandleDeleteCommonCommandAction, enum.PermissionCommandWrite))
 	api.HandleAPIMethod(api.GET, "/elasticsearch/overview/status", handler.RequireLogin(handler.ElasticsearchStatusSummaryAction))
 	api.HandleAPIMethod(api.GET, "/elasticsearch/:id/overview/treemap", handler.RequireClusterPermission(handler.RequirePermission(handler.ClusterOverTreeMap, enum.PermissionElasticsearchMetricRead)))
-	//task.RegisterScheduleTask(task.ScheduleTask{
-	//	Description: "sync reindex task result",
-	//	Task: func() {
-	//		err := index_management.SyncRebuildResult(cfg.Elasticsearch)
-	//		if err != nil {
-	//			log.Error(err)
-	//		}
-	//	},
-	//})
 
 	alertAPI := alerting.AlertAPI{}
 

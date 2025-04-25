8ebc62144815a2af1ef571461373793b33d08d1b
hardy <luohf@infinilabs.com>
2025-04-25 19:01:54 +0800

    fix: init cluster url

--- web/src/pages/Guide/Initialization/components/Finish/index.js
+++ web/src/pages/Guide/Initialization/components/Finish/index.js
@@ -13,7 +13,7 @@ export default ({ formData }) => {
     username,
     password,
     isInit,
-      hosts = [],
+    hosts = [],
     bootstrap_username,
     bootstrap_password,
     credential_secret,
@@ -83,7 +83,7 @@ export default ({ formData }) => {
           title={formatMessage({ id: "guide.configuration.title" })} 
           column={1}
         >
-          <Descriptions.Item label={formatMessage({ id: "guide.configuration.cluster" })}>{isTLS ? `https://${host}` : `http://${host}`}</Descriptions.Item>
+          <Descriptions.Item label={formatMessage({ id: "guide.configuration.cluster" })}>{isTLS ? `https://${hostV}` : `http://${hostV}`}</Descriptions.Item>
           {
             isAuth && (
               <>
